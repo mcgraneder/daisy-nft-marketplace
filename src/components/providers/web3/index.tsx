27eca83f38b5fff301f0a42ef1f29674ba066c61
mcgraneder <mcgrane480@gmail.com>
2023-03-04 21:31:12 +0000

    feat: added web3 custom provider

---
+++ src/components/providers/web3/index.tsx
@@ -0,0 +1,89 @@
+import {MetaMaskInpageProvider} from '@metamask/providers';
+import {NftMarketContract} from '@_types/nftMarketContract';
+import {ethers} from 'ethers';
+import {createContext, FC, useContext, useEffect, useState} from 'react';
+import {
+	createDefaultState,
+	createWeb3State,
+	loadContract,
+	Web3State,
+} from './utils';
+
+const Web3Context = createContext<Web3State>(createDefaultState());
+
+interface IWeb3ProviderProps {
+	children: React.ReactNode;
+}
+
+const pageReload = () => window.location.reload();
+const handleAccount = (ethereum: MetaMaskInpageProvider) => async () => {
+	pageReload();
+	const isLocked = !(await ethereum._metamask.isUnlocked());
+	if (isLocked) {
+	}
+};
+const setGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
+	ethereum.on('chainChanged', pageReload);
+	ethereum.on('accountsChanged', handleAccount(ethereum));
+};
+const removeGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
+	ethereum?.removeListener('chainChanged', pageReload);
+	ethereum?.removeListener('accountsChanged', handleAccount);
+};
+export const Web3Provider: FC<IWeb3ProviderProps> = ({children}) => {
+	const [web3Api, setWeb3Api] = useState<Web3State>(createDefaultState());
+
+	useEffect(() => {
+		(async function () {
+			try {
+				const {ethereum} = window;
+
+				const provider = new ethers.providers.Web3Provider(ethereum as any);
+
+				const contract = await loadContract('NftMarket', provider);
+
+				const signer = provider.getSigner(); // this is for provider to know which account to use for transactions
+				const signedContract = contract.connect(signer);
+				if (ethereum) {
+					setGlobalListeners(ethereum);
+					setWeb3Api(
+						createWeb3State({
+							isLoading: false,
+							ethereum,
+							provider,
+							contract: signedContract as unknown as NftMarketContract,
+						}),
+					);
+				} else {
+					alert('Please install MetaMask');
+				}
+			} catch (error) {
+				setWeb3Api(() => {
+					return {
+						...createDefaultState(),
+						isLoading: false,
+					};
+				});
+			}
+		})();
+
+		// async function initWeb3() {}
+		// initWeb3();
+		return () => {
+			removeGlobalListeners(window.ethereum);
+		};
+	}, []);
+
+	return (
+		<Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
+	);
+};
+
+export function useWeb3() {
+	return useContext(Web3Context);
+}
+
+export function useHooks() {
+	const {hooks} = useWeb3();
+	return hooks;
+}
